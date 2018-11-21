ccc4ecf998170e736f318d4017ffcd90180f2ad7
Philip Shamsiev <ph.shamsiev@mail.ru>
2018-11-21 16:10:28 +0300

    Add app styling

--- src/components/Action.js
+++ src/components/Action.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Action = (props) => (
   <div>
     <button
+      className="big-button"
       disabled={!props.hasOptions}
       onClick={props.handlePick}
     >
--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -73,18 +73,22 @@ export default class IndecisionApp extends React.Component {
     return (
       <div>
         <Header subtitle={subtitle} />
-        <Action
-          hasOptions={this.state.options.length > 0}
-          handlePick={this.handlePick}
-        />
-        <Options
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-          options={this.state.options}
-        />
-        <AddOption
-          handleAddOption={this.handleAddOption}
-        />
+        <div className="container">
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick}
+          />
+          <div className="widget">
+            <Options
+              handleDeleteOptions={this.handleDeleteOptions}
+              handleDeleteOption={this.handleDeleteOption}
+              options={this.state.options}
+            />
+            <AddOption
+              handleAddOption={this.handleAddOption}
+            />
+          </div>
+        </div>
         <OptionModal
           selectedOption={this.state.selectedOption}
           handleClearSelectedOption={this.handleClearSelectedOption}
