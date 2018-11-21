583f68b247e5a212c337c84b133676820f366f1f
Philip Shamsiev <ph.shamsiev@mail.ru>
2018-11-21 12:03:36 +0300

    Refactor stateless functions: remove return statement

--- src/components/Options.js
+++ src/components/Options.js
@@ -2,22 +2,20 @@ import React from 'react';
 
 import Option from './Option';
 
-const Options = (props) => {
-  return (
-    <div>
-      <button onClick={props.handleDeleteOptions}>Remove All</button>
-      {props.options.length === 0 && <p>Please add an option to get started!</p>}
-      {
-        props.options.map((option, i) => (
-          <Option
-            key={i}
-            optionText={option}
-            handleDeleteOption={props.handleDeleteOption}
-          />
-        ))
-      }
-    </div>
-  );
-};
+const Options = (props) => (
+  <div>
+    <button onClick={props.handleDeleteOptions}>Remove All</button>
+    {props.options.length === 0 && <p>Please add an option to get started!</p>}
+    {
+      props.options.map((option, i) => (
+        <Option
+          key={i}
+          optionText={option}
+          handleDeleteOption={props.handleDeleteOption}
+        />
+      ))
+    }
+  </div>
+);
 
 export default Options;
