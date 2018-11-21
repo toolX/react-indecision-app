661f510cf8e8cfa5bb65a04352d75069faba484b
Philip Shamsiev <ph.shamsiev@mail.ru>
2018-11-21 11:57:09 +0300

    Creat OptionModal component with react-modal

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -4,10 +4,12 @@ import Header from './Header';
 import Action from './Action';
 import AddOption from './AddOption';
 import Options from './Options';
+import OptionModal from './OptionModal';
 
 export default class IndecisionApp extends React.Component {
   state = {
-    options: []
+    options: [],
+    selectedOption: undefined
   };
 
   componentDidMount() {
@@ -47,6 +49,7 @@ export default class IndecisionApp extends React.Component {
   handlePick = () => {
     const randomNumber = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNumber];
+    this.setState(() => ({ selectedOption: option }));
     console.log(option);
   };
 
@@ -58,6 +61,10 @@ export default class IndecisionApp extends React.Component {
     }
 
     this.setState((prevState) => ({ options: prevState.options.concat(option) }));
+  };
+
+  handleClearSelectedOption = () => {
+    this.setState(() => ({ selectedOption: undefined }));
   };
 
   render() {
@@ -77,6 +84,10 @@ export default class IndecisionApp extends React.Component {
         />
         <AddOption
           handleAddOption={this.handleAddOption}
+        />
+        <OptionModal
+          selectedOption={this.state.selectedOption}
+          handleClearSelectedOption={this.handleClearSelectedOption}
         />
       </div>
     );
