3230f2db15ee3c0a36bf109a8dde340c9cbbabd9
Philip Shamsiev <ph.shamsiev@mail.ru>
2018-11-21 11:11:23 +0300

    Fix es6 classes properties and methods

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -6,18 +6,9 @@ import AddOption from './AddOption';
 import Options from './Options';
 
 export default class IndecisionApp extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-    this.handleDeleteOption = this.handleDeleteOption.bind(this);
-    this.handlePick = this.handlePick.bind(this);
-    this.handleAddOption = this.handleAddOption.bind(this);
-
-    this.state = {
-      options: []
-    };
-  }
+  state = {
+    options: []
+  };
 
   componentDidMount() {
     try {
@@ -43,23 +34,23 @@ export default class IndecisionApp extends React.Component {
     console.log('Will unmount');
   }
 
-  handleDeleteOptions() {
+  handleDeleteOptions = () => {
     this.setState(() => ({ options: [] }));
-  }
+  };
 
-  handleDeleteOption(optionToRemove) {
+  handleDeleteOption = (optionToRemove) => {
     this.setState((prevState) => ({
       options: prevState.options.filter((option) => optionToRemove !== option)
     }));
-  }
+  };
 
-  handlePick() {
+  handlePick = () => {
     const randomNumber = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNumber];
     console.log(option);
-  }
+  };
 
-  handleAddOption(option) {
+  handleAddOption = (option) => {
     if (!option) {
       return 'Enter valid value to add item';
     } else if (this.state.options.indexOf(option) > -1) {
@@ -67,7 +58,7 @@ export default class IndecisionApp extends React.Component {
     }
 
     this.setState((prevState) => ({ options: prevState.options.concat(option) }));
-  }
+  };
 
   render() {
     const subtitle = 'Put your life in the hands of a computer.';
